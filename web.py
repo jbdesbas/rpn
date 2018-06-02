22cb1ab37161708256dd42921c387d58b761f4b5
jbdesbas <jb.desbas@gmail.com>
2018-06-02 19:22:34 +0200

    upload fichier

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, flash
+import os,uuid
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -10,7 +12,14 @@ def form_data():
     if request.method == 'GET':
         return render_template('formLoadData.html')
     else :
-        return 'traiter le fichier envoyé'
+        file = request.files['input_file']
+        file.save('data/'+str(uuid.uuid4()))
+        flash('hahah')
+        return 'traiter le fichier envoyé '+file.filename
+
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
+    app.config['SESSION_TYPE'] = 'filesystem'
+
     app.run(debug=True, host='0.0.0.0', port=4999)
