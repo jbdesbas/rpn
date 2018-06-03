0efe3c78a60b05e3de87c115ed53bd156142f8ec
jbdesbas <jb.desbas@gmail.com>
2018-06-03 09:41:53 +0200

    upload fichier zip

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request, flash
 import os,uuid
+from zipfile import ZipFile
 app = Flask(__name__)
 
 
@@ -13,9 +14,16 @@ def form_data():
         return render_template('formLoadData.html')
     else :
         file = request.files['input_file']
-        file.save('data/'+str(uuid.uuid4()))
-        flash('hahah')
-        return 'traiter le fichier envoyé '+file.filename
+        uniquename='data/'+str(uuid.uuid4())
+        if file.filename.endswith('.zip'):
+            z=ZipFile(file)
+            for f in z.namelist():
+                with open(uniquename+'.'+f.split('.')[-1],'wb') as dest :
+                    dest.write(z.open(f).read())
+            return str(z.namelist())
+        else:
+            file.save(uniquename+'.'+file.filename.split('.')[-1])
+        return 'fichier sauvegarde '+file.filename
 
 
 if __name__ == '__main__':
